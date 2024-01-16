f3dbc7128cbdedd263092af07e4a730e6967899e
rahmatadlin <rahmatadlin210@gmail.com>
2024-01-16 22:59:35 +0700

    Server & Client Side: Login and Register

---
+++ prevferr/src/types/types.d.ts
@@ -0,0 +1,16 @@
+// User
+type TGETUsers = {
+    id: number;
+    fistname: string,
+    lastname: string,
+    password: string,
+    education: string,
+    scope: string,
+    institution: string,
+    profileImage: string,
+    email: string,
+    background: string,
+    gender: string,
+    role: string,
+    location: string
+  };
