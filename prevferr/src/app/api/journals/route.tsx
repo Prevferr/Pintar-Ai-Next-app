b52252136ceae22ddd4e0a2befd53c8268073864
Aisyah Aulia Angelinee <vincentius.clarishna@gmail.com>
2024-01-18 19:15:42 +0700

    feat: create abstract

--- prevferr/src/app/api/journals/route.tsx
+++ prevferr/src/app/api/journals/route.tsx
@@ -6,7 +6,7 @@ console.log(prisma, "<<< prismanya");
 import * as bcryptjs from "bcryptjs";
 
 export async function GET(req: NextRequest) {
-	const journals = await prisma.journal.findMany();
+	const journals = await prisma.jurnal.findMany();
 	console.log(journals, "<<< journal");
 
 	return NextResponse.json(journals);
---
+++ prevferr/src/app/create-journal/page.tsx
@@ -0,0 +1,71 @@
+// import { handleFormAction } from "./action";
+"use client";
+
+import { ChangeEvent, useState } from "react";
+
+const page = () => {
+	const [loading, setLoading] = useState(false);
+	const [form, setForm] = useState({
+		abstract: "",
+		title: "",
+		description: "",
+	});
+	const [error, setError] = useState("");
+
+	const onSubmit = async (e: React.FormEvent) => {
+		e.preventDefault();
+		setLoading(true);
+		setForm({
+			abstract: "",
+			title: "",
+			description: "",
+		});
+
+		try {
+			const response = await fetch("/api/journals", {
+				method: "POST",
+				body: JSON.stringify(form),
+				headers: {
+					"Content-Type": "application/json",
+				},
+			});
+
+			setLoading(false);
+			if (!response.ok) {
+				setError(await response.json());
+				return;
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	const handleChangeForm = (event: ChangeEvent<HTMLInputElement>) => {
+		console.log(event.target, "<<<");
+	};
+
+	return (
+		<>
+			<div>Create Journals</div>
+			<form className="max-w-md mx-auto" onSubmit={onSubmit}>
+				<div className="relative z-0 w-full mb-5 group">
+					<input type="text" name="abstract" id="floating_abstrack" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " onChange={handleChangeForm} value={form.abstract} />
+					<label className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Journal Abstrack</label>
+				</div>
+				<div className="relative z-0 w-full mb-5 group">
+					<input type="text" name="title" id="floating_title" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " onChange={handleChangeForm} value={form.title} />
+					<label className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Journal Title</label>
+				</div>
+				<div className="relative z-0 w-full mb-5 group">
+					<input type="text" name="description" id="floating_description" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " onChange={handleChangeForm} value={form.description} />
+					<label className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Journal Description</label>
+				</div>
+				<button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+					Upload
+				</button>
+			</form>
+		</>
+	);
+};
+
+export default page;
