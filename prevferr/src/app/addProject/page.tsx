141d7e75651e7f08f62f67d1d4e4a5721131faaf
Aisyah Aulia Angelinee <vincentius.clarishna@gmail.com>
2024-01-19 17:56:10 +0700

    feat: create project

--- prevferr/src/app/addProject/page.tsx
+++ prevferr/src/app/addProject/page.tsx
@@ -7,7 +7,11 @@ const AddProjectForm = () => {
 		project_name: "",
 		description_project: "",
 		project_image: "",
+		project_status: false,
+		starting_date: "",
+		expected_finish_date: "",
 		project_budget: "",
+		tags: "",
 	});
 	const [error, setError] = useState("");
 
@@ -17,7 +21,11 @@ const AddProjectForm = () => {
 			project_name: "",
 			description_project: "",
 			project_image: "",
+			project_status: false,
+			starting_date: "",
+			expected_finish_date: "",
 			project_budget: "",
+			tags: "",
 		});
 
 		try {
@@ -28,7 +36,8 @@ const AddProjectForm = () => {
 					"Content-Type": "application/json",
 				},
 			});
-			console.log(response.json(), "<<<< response project");
+			const responseJSON = await response.json();
+			console.log(responseJSON, "<<<< response project");
 		} catch (error: any) {
 			console.log(error);
 			setError(error);
@@ -54,7 +63,16 @@ const AddProjectForm = () => {
 					<input type="text" name="project_image" placeholder="Enter Project Image URL" value={form.project_image} onChange={onHandlerForm} />
 				</div>
 				<div>
+					<input type="date" name="starting_date" value={form.starting_date} onChange={onHandlerForm} />
+				</div>
+				<div>
+					<input type="date" name="expected_finish_date" value={form.expected_finish_date} onChange={onHandlerForm} />
+				</div>
+				<div>
 					<input type="number" name="project_budget" placeholder="Enter Project Budget" value={form.project_budget} onChange={onHandlerForm} />
+				</div>
+				<div>
+					<input type="text" name="tags" placeholder="Enter Project Tags" value={form.tags} onChange={onHandlerForm} />
 				</div>
 				<button type="submit">Create Project</button>
 			</form>
--- prevferr/src/app/api/projects/route.ts
+++ prevferr/src/app/api/projects/route.ts
@@ -1,31 +1,55 @@
 import { NextResponse } from "next/server";
 import { prisma } from "../../../../helpers/lib/prisma";
+import { number, z } from "zod";
 
 export async function POST(req: Request) {
 	try {
-		const { project_name, description_project, project_image, project_budget, tags } = (await req.json()) as {
+		const { project_name, description_project, project_image, project_status, starting_date, expected_finish_date, project_budget, tags } = (await req.json()) as {
 			project_name: string;
 			description_project: string;
 			project_image: string;
+			project_status: boolean;
+			starting_date: Date;
+			expected_finish_date: Date;
 			project_budget: number;
 			tags: string;
 		};
+
+		// const projectInput = z.object({
+		// 	project_name: z.string(),
+		// 	description_project: z.string(),
+		// 	project_image: z.string(),
+		// 	project_status: z.boolean(),
+		// 	starting_date: z.date(),
+		// 	expected_finish_date: z.date(),
+		// 	project_budget: z.number(),
+		// 	tags: z.string(),
+		// });
+
+		// console.log({ starting_date, expected_finish_date });
 
 		const project = await prisma.project.create({
 			data: {
 				project_name,
 				description_project,
 				project_image,
-				project_budget,
+				project_status,
+				starting_date: new Date(starting_date),
+				expected_finish_date: new Date(expected_finish_date),
+				project_budget: Number(project_budget),
 				tags,
 			},
 		});
+		// console.log(project, "<<< project");
 
 		return NextResponse.json({
 			project: {
 				project_name: project.project_name,
 				description_project: project.description_project,
 				project_image: project.project_image,
+				project_status: project.project_status,
+				starting_date: project.starting_date,
+				expected_finish_date: project.expected_finish_date,
 				project_budget: project.project_budget,
 				tags: project.tags,
 			},
