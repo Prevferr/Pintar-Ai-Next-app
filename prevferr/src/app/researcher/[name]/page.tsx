98fa2d826cf97bfa5cfbfbb6d807120437d1bd52
Alvin Pratama <palveeen22@gmail.com>
2024-01-20 14:21:09 +0700

    feat: profile added, status: need fix

--- prevferr/src/app/researcher/[name]/page.tsx
+++ prevferr/src/app/researcher/[name]/page.tsx
@@ -3,9 +3,16 @@ import React from "react";
 
 const page = () => {
 	return (
-		<section className="w-full bg-[#E2E4DD] flex min-h-screen">
-			<div className="w-[30%] bg-red-400 ">profile</div>
-			<div className="w-[70%] flex flex-col">
+		<section className="w-full bg-[#E2E4DD] flex">
+			<div className="w-[20%]  paddingXShorter2 paddingYShorter2 flex flex-col items-center gap-4">
+				<img
+					src="https://cdn.sanity.io/images/2ejqxsnu/production/d6d798e8581a361efb9d9ef2923794da065d0e6e-450x445.jpg?w=128&q=75&fit=clip&auto=format"
+					className=" w-60 h-60 rounded-full object-cover"
+					alt="researcher"
+				/>
+				<p className="text-base font-mono">Adam Turnere</p>
+			</div>
+			<div className="w-[80%] flex flex-col">
 				<JournalCard />
 				<JournalCard />
 				<JournalCard />
