ce8af57eb0864954c976aa5e74b9f53a74fbb576
Aisyah Aulia Angelinee <vincentius.clarishna@gmail.com>
2024-01-21 20:48:01 +0700

    commit

--- prevferr/helpers/lib/auth.ts
+++ prevferr/helpers/lib/auth.ts
@@ -8,74 +8,73 @@ import { prisma } from "./prisma";
 import { compare } from "bcryptjs";
 
 export const authOptions: NextAuthOptions = {
-  // This is a temporary fix for prisma client.
-  // @see https://github.com/prisma/prisma/issues/16117
-  adapter: PrismaAdapter(prisma as any),
-  pages: {
-    signIn: "/loginn",
-  },
-  session: {
-    strategy: "jwt",
-  },
-  providers: [
-    CredentialsProvider({
-      name: "Sign in",
-      id: "credentials",
-      credentials: {
-        email: {
-          label: "Email",
-          type: "email",
-          placeholder: "example@example.com",
-        },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials) {
-        if (!credentials?.email || !credentials.password) {
-          return null;
-        }
+	// This is a temporary fix for prisma client.
+	// @see https://github.com/prisma/prisma/issues/16117
+	adapter: PrismaAdapter(prisma as any),
+	pages: {
+		signIn: "/loginn",
+	},
+	session: {
+		strategy: "jwt",
+	},
+	providers: [
+		CredentialsProvider({
+			name: "Sign in",
+			id: "credentials",
+			credentials: {
+				email: {
+					label: "Email",
+					type: "email",
+					placeholder: "example@example.com",
+				},
+				password: { label: "Password", type: "password" },
+			},
+			async authorize(credentials) {
+				if (!credentials?.email || !credentials.password) {
+					return null;
+				}
 
-        const user = await prisma.researcher.findUnique({
-          where: {
-            email: credentials.email,
-          },
-        });
+				const user = await prisma.researcher.findUnique({
+					where: {
+						email: credentials.email,
+					},
+				});
 
-        if (!user || !(await compare(credentials.password, user.password!))) {
-          return null;
-        }
+				if (!user || !(await compare(credentials.password, user.password!))) {
+					return null;
+				}
 
-        return {
-          id: user.id + "",
-          email: user.email,
-          firstname: user.firstname,
-          randomKey: "Hey cool",
-        };
-      },
-    }),
-  ],
-  callbacks: {
-    session: ({ session, token }) => {
-      return {
-        ...session,
-        user: {
-          ...session.user,
-          id: token.id,
-          randomKey: token.randomKey,
-        },
-      };
-    },
-    jwt: ({ token, user }) => {
-      if (user) {
-        const u = user as unknown as any;
-        return {
-          ...token,
-          id: u.id,
-          randomKey: u.randomKey,
-        };
-      }
-      return token;
-    },
-  },
-  secret: process.env.NEXTAUTH_SECRET
+				return {
+					id: user.id + "",
+					email: user.email,
+					firstname: user.firstname,
+					randomKey: "Hey cool",
+				};
+			},
+		}),
+	],
+	callbacks: {
+		session: ({ session, token }) => {
+			return {
+				...session,
+				user: {
+					...session.user,
+					id: token.id,
+					randomKey: token.randomKey,
+				},
+			};
+		},
+		jwt: ({ token, user }) => {
+			if (user) {
+				const u = user as unknown as any;
+				return {
+					...token,
+					id: u.id,
+					randomKey: u.randomKey,
+				};
+			}
+			return token;
+		},
+	},
+	secret: process.env.NEXTAUTH_SECRET,
 };
-
