107baacffd21193a5f7352885f2d23c3df3ecf9c
Alvin Pratama <palveeen22@gmail.com>
2024-01-21 15:13:04 +0700

    feat: change pdf to mook data picture

--- prevferr/src/app/components/JournalCard.tsx
+++ prevferr/src/app/components/JournalCard.tsx
@@ -4,7 +4,7 @@ const JournalCard = () => {
 	return (
 		<div className="w-full border-x border-[#000] cursor-pointer">
 			<div className="border-[#000] border-b h-48 flex justify-start gap-8 p-4 hover:bg-[#fff]">
-				<img src="https://cdn.sanity.io/images/2ejqxsnu/production/549f2b7b43d7cf94979e9061ec99a6c6e34a5f83-1200x760.png?w=750&q=75&fit=clip&auto=format" />
+				<img src="https://wp-pdf.com/wp-content/uploads/2021/07/pdf-image.png" />
 				<div className="w-[40%] flex flex-col gap-4">
 					<p className="font-mono text-[#565e67]">
 						NOVEMBER 27, 2023 (ABOUT 2 MONTHS AGO)
