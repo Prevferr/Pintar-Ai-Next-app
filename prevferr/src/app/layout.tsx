d79a80e7b9ebd43eaff5397f246e264c2a41fc6e
Aisyah Aulia Angelinee <vincentius.clarishna@gmail.com>
2024-01-21 16:33:07 +0700

    edgestore setup

--- prevferr/src/app/layout.tsx
+++ prevferr/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { EdgeStoreProvider } from "@/lib/edgestore";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,15 +11,13 @@ export const metadata: Metadata = {
 	// icons
 };
 
-export default function RootLayout({
-	children,
-}: {
-	children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang="en">
-			<body className={inter.className}>{children}</body>
+			<body className={inter.className}>
+				<EdgeStoreProvider>{children}</EdgeStoreProvider>
+			</body>
 		</html>
 	);
 }
-4
+4;
