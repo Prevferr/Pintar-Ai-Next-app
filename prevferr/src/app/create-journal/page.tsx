6ecc23c74b582a8a39ef91a5ed68ee045177987a
Aisyah Aulia Angelinee <vincentius.clarishna@gmail.com>
2024-01-22 12:58:29 +0700

    commit

--- prevferr/src/app/create-journal/page.tsx
+++ prevferr/src/app/create-journal/page.tsx
@@ -1,24 +1,9 @@
 "use client";
 
-import { useEdgeStore } from "@/lib/edgestore";
 import Link from "next/link";
 import React, { useState } from "react";
-import { SingleImageDropzone } from "../components/singe-image-dropzone";
 
 const AddJournalForm = () => {
-	// Edge store state
-	const [file, setFile] = useState<File>();
-	const [progress, setProgress] = useState(0);
-
-	// const [urls, setUrls] = useState<{
-	// 	url: string;
-	// 	thumbnailUrl: string | null;
-	// }>();
-
-	const [urls, setUrls] = useState<{
-		url: string;
-	}>();
-
 	const [form, setForm] = useState({
 		abstract: "",
 		title: "",
@@ -55,9 +40,6 @@ const AddJournalForm = () => {
 		setForm({ ...form, [name]: value });
 	};
 
-	// Edge store setup
-	const { edgestore } = useEdgeStore();
-
 	return (
 		<>
 			<form onSubmit={onSubmit}>
@@ -72,70 +54,8 @@ const AddJournalForm = () => {
 				</div>
 				<button type="submit">Create Journal</button>
 			</form>
-			<div className="flex flex-col items-center m-6 gap-2">
-				<SingleImageDropzone
-					width={200}
-					height={200}
-					value={file}
-					onChange={(file) => {
-						setFile(file);
-					}}
-				/>
-				<div className="h-[6px] w-44 border rounded overflow-hidden">
-					<div className="h-full bg-black transition-all duration-150" style={{ width: `${progress}%` }} />
-				</div>
-				<button
-					className="bg-white text-black rounded px-2 hover:opacity-80"
-					onClick={async () => {
-						if (file) {
-							const res = await edgestore.publicFiles.upload({
-								file,
-								onProgressChange: (progress) => {
-									setProgress(progress);
-								},
-							});
-							setUrls({ url: res.url });
-						}
-					}}
-				>
-					Upload Journal
-				</button>
-				{urls?.url && (
-					<Link href={urls.url} target="_blank">
-						URL
-					</Link>
-				)}
-				{/* {urls?.thumbnailUrl && (
-					<Link href={urls.thumbnailUrl} target="_blank">
-						THUMBNAIL
-					</Link>
-				)} */}
-			</div>
 		</>
 	);
 };
 
 export default AddJournalForm;
-
-{
-	/* <button
-	className="bg-white text-black rounded px-2 hover:opacity-80"
-	onClick={async () => {
-		if (file) {
-			const res = await edgestore.myPublicImages.upload({
-				file,
-				input: { type: "post" },
-				onProgressChange: (progress) => {
-					setProgress(progress);
-				},
-			});
-			setUrls({
-				url: res.url,
-				thumbnailUrl: res.thumbnailUrl,
-			});
-		}
-	}}
->
-	Upload Journal
-</button>; */
-}
