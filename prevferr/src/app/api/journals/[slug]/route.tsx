5559eefc782e9717f9d3a6d45a4a5c51cf7eff71
Aisyah Aulia Angelinee <vincentius.clarishna@gmail.com>
2024-01-22 10:39:21 +0700

    commit

--- prevferr/src/app/api/journals/[slug]/route.tsx
+++ prevferr/src/app/api/journals/[slug]/route.tsx
@@ -1,25 +1,23 @@
 import { prisma } from "../../../../../helpers/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
-
 type TProps = {
-  params: { title: string };
+	params: { title: string };
 };
 
 // GET journal by title
 export async function GET(req: NextRequest, { params }: TProps) {
-  
-  const project = await prisma.jurnal.findFirst({
-    where: { title: params.title },
-    include: {
-      portofolio: true,
-    },
-  });
+	const project = await prisma.jurnal.findFirst({
+		where: { title: params.title },
+		include: {
+			portofolio: true,
+		},
+	});
 
-  if (!project) {
-    return NextResponse.json({ error: "Journal not found" }, { status: 404 });
-  }
+	if (!project) {
+		return NextResponse.json({ error: "Journal not found" }, { status: 404 });
+	}
 
-  return NextResponse.json(project);
+	return NextResponse.json(project);
 }
 // const slugmodified = slug.replace(/%20/g, " ")
