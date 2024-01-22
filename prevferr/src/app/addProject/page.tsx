d22098a1d7031d0d3a0254b845db89ece034061d
Aisyah Aulia Angelinee <vincentius.clarishna@gmail.com>
2024-01-22 23:39:34 +0700

    commit

--- prevferr/src/app/addProject/page.tsx
+++ prevferr/src/app/addProject/page.tsx
@@ -58,29 +58,42 @@ const AddProjectForm = () => {
 
 	return (
 		<>
-			<form onSubmit={onSubmit}>
-				<div>
-					<input type="text" name="project_name" placeholder="Enter Project Name" value={form.project_name} onChange={onHandlerForm} />
+			<form className="max-w-md mx-auto pt-9 pb-9" onSubmit={onSubmit}>
+				<div className="relative z-0 w-full mb-5 group">
+					<input type="text" name="project_name" id="project_name" className="block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " value={form.project_name} onChange={onHandlerForm} />
+					<label className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Project Name</label>
 				</div>
-				<div>
-					<input type="text" name="description_project" placeholder="Enter Project Description" value={form.description_project} onChange={onHandlerForm} />
+				<div className="relative z-0 w-full mb-5 group">
+					<input type="text" name="description_project" id="description_project" className="block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " value={form.description_project} onChange={onHandlerForm} />
+					<label className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Project Description</label>
 				</div>
-				<div>
-					<input type="text" name="project_image" placeholder="Enter Project Image URL" value={form.project_image} onChange={onHandlerForm} />
+				<div className="relative z-0 w-full mb-5 group">
+					<input type="text" name="project_image" id="project_image" className="block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " value={form.project_image} onChange={onHandlerForm} />
+					<label className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Project Image URL</label>
 				</div>
-				<div>
-					<input type="date" name="starting_date" value={form.starting_date} onChange={onHandlerForm} />
+				<div className="grid md:grid-cols-2 md:gap-6">
+					<div className="relative z-0 w-full mb-5 group">
+						<input type="date" name="starting_date" id="starting_date" className="block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " value={form.starting_date} onChange={onHandlerForm} />
+						<label className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Starting Date</label>
+					</div>
+					<div className="relative z-0 w-full mb-5 group">
+						<input type="date" name="expected_finish_date" id="expected_finish_date" className="block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " value={form.expected_finish_date} onChange={onHandlerForm} />
+						<label className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Expected Finish Date</label>
+					</div>
 				</div>
-				<div>
-					<input type="date" name="expected_finish_date" value={form.expected_finish_date} onChange={onHandlerForm} />
+				<div className="grid md:grid-cols-2 md:gap-6">
+					<div className="relative z-0 w-full mb-5 group">
+						<input type="number" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" name="project_budget" id="project_budget" className="block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " value={form.project_budget} onChange={onHandlerForm} />
+						<label className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Project Budget</label>
+					</div>
+					<div className="relative z-0 w-full mb-5 group">
+						<input type="text" name="tags" id="tags" className="block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " value={form.tags} onChange={onHandlerForm} />
+						<label className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Tags</label>
+					</div>
 				</div>
-				<div>
-					<input type="number" name="project_budget" placeholder="Enter Project Budget" value={form.project_budget} onChange={onHandlerForm} />
-				</div>
-				<div>
-					<input type="text" name="tags" placeholder="Enter Project Tags" value={form.tags} onChange={onHandlerForm} />
-				</div>
-				<button type="submit">Create Project</button>
+				<button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+					Create Project
+				</button>
 			</form>
 		</>
 	);
