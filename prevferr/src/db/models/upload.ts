e283d38028de90ebfd695cad7e9fd6e28f3572e3
Alvin Pratama <palveeen22@gmail.com>
2024-01-23 10:52:28 +0700

    fix: bug on parse pdf

--- prevferr/src/db/models/upload.ts
+++ prevferr/src/db/models/upload.ts
@@ -4,6 +4,7 @@ import { prisma } from "../../../helpers/lib/prisma";
 // import { getDownloadURL, ref, uploadBytes } from 'firebase/storage'
 import { PdfReader } from "pdfreader";
 
+
 export class Upload {
 	static async extractPDF(file: File, userId:any) {
 		try {
@@ -12,54 +13,55 @@ export class Upload {
       const result: string[] = []
 
       const pdfreader = new PdfReader({})
-      pdfreader.parseBuffer(fileBuff, (err, item) => {
-        if (err) console.error('error:', err)
-        else if (!item) return this.sumPDF(result.join(' '), userId)
-        else if (item.text) {
-          result.push(item.text)
-        }
+            pdfreader.parseBuffer(fileBuff, (err: any, item: any) => {
+          console.log(item, "<<<<?");
+        // if (err) console.error('error:', err)
+        // else if (!item) return this.sumPDF(result.join(' '), userId)
+        // else if (item.text) {
+        //   result.push(item.text)
+        // }
+        //   console.log(item, "<<<<< masuk ga");
       })
             
-            console.log(pdfreader, "memek");
     } catch (err) {
       console.log(err)
     }
 	}
 
-	static async sumPDF(val: string, req: Request) {
-		try {
-			const userId = req.headers.get("x-user-id") as string;
+	// static async sumPDF(val: string, req: Request) {
+	// 	try {
+	// 		const userId = req.headers.get("x-user-id") as string;
 
-			const ai = await openai.chat.completions.create({
-				model: "gpt-4",
-				messages: [
-					{
-						role: "user",
-						content: `can you summarize this pdf file in part ABSTRACT into four exact points divided by a dash in a line, that is title, his keyword and point of research and abstract description of research ${val}`,
-					},
-				],
-			});
+	// 		const ai = await openai.chat.completions.create({
+	// 			model: "gpt-4",
+	// 			messages: [
+	// 				{
+	// 					role: "user",
+	// 					content: `can you summarize this pdf file in part ABSTRACT into four exact points divided by a dash in a line, that is title, his keyword and point of research and abstract description of research ${val}`,
+	// 				},
+	// 			],
+	// 		});
 
-            const data = (ai.choices[0].message.content as string)
+    //         const data = (ai.choices[0].message.content as string)
             
 
-			const title = data[1];
-			const abstract = data[2];
+	// 		const title = data[1];
+	// 		const abstract = data[2];
 
-			// Prisma create query
-			await prisma.jurnal.create({
-				data: {
-					researcherId: Number(userId),
-					title,
-					abstract,
-				},
-			});
+	// 		// Prisma create query
+	// 		await prisma.jurnal.create({
+	// 			data: {
+	// 				researcherId: Number(userId),
+	// 				title,
+	// 				abstract,
+	// 			},
+	// 		});
 
-			return data;
-		} catch (err) {
-			console.error(err);
-		}
-	}
+	// 		return data;
+	// 	} catch (err) {
+	// 		console.error(err);
+	// 	}
+	// }
 
 	//   await collection.findOneAndUpdate(
 	//     { _id: new ObjectId(userId) },
