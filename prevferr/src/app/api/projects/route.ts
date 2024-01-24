04ad04c776ee11306bad9c031642477644cb39b3
rahmatadlin <rahmatadlin210@gmail.com>
2024-01-24 16:41:15 +0700

    Bang udah bang hehe

--- prevferr/src/app/api/projects/route.ts
+++ prevferr/src/app/api/projects/route.ts
@@ -58,22 +58,37 @@ export async function POST(req: Request) {
 }
 
 // GET ALL PROJECTS
+// export async function GET(req: NextRequest) {
+// 	// console.log("masuk di get /api/projects");
+// 	// console.log(req.headers.get("x-user-id"), "<<<< user id nih di get");
+
+// 	const idUserLogin = req.headers.get("x-user-id");
+// 	// console.log(idUserLogin, "WKOAKWOKAOWKAW");
+
+// 	const projects = await prisma.project.findMany({
+// 		where: {
+// 			investorId: Number(idUserLogin),
+// 		},
+// 	});
+// 	// console.log(projects, "<<<<<<< PROJECTT");
+
+// 	return NextResponse.json(projects);
+// }
+
+// GET ALL PROJECTS
 export async function GET(req: NextRequest) {
 	// console.log("masuk di get /api/projects");
 	// console.log(req.headers.get("x-user-id"), "<<<< user id nih di get");
 
-	const idUserLogin = req.headers.get("x-user-id");
+	// const idUserLogin = req.headers.get("x-user-id");
 	// console.log(idUserLogin, "WKOAKWOKAOWKAW");
 
-	const projects = await prisma.project.findMany({
-		where: {
-			investorId: Number(idUserLogin),
-		},
-	});
+	const projects = await prisma.project.findMany();
 	// console.log(projects, "<<<<<<< PROJECTT");
 
 	return NextResponse.json(projects);
 }
+
 
 // GET ALL RESEARCHER
 // export async function GetAllResearchers(req: NextRequest) {
