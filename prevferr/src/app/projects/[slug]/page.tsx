b6f5f0659af81b049f8b8c1e678adde54f7b8b6a
Aisyah Aulia Angelinee <vincentius.clarishna@gmail.com>
2024-01-24 19:21:16 +0700

    commit

--- prevferr/src/app/projects/[slug]/page.tsx
+++ prevferr/src/app/projects/[slug]/page.tsx
@@ -47,25 +47,32 @@ const ProjectDetail = ({ params }: { params: { slug: string } }) => {
 
 	return (
 		<>
-			<section className="min-h-screen bg-[#E2E4DD]">
-				<div className="paddingX paddingYShorter2">
-					<div className="w-[60%] mx-auto flex flex-col gap-4">
-						<p className="font-mono text-[#565e67]">{projectData?.starting_date}</p>
-						<h1 className="text-5xl">{projectData?.project_name}</h1>
-						<div className="flex justify-start items-center gap-4">
-							{/* <img src={projectData?.project_image} className="h-14 w-14 rounded-full object-cover" /> */}
-							<p className="text-base font-mono">
-								<span className="font-mono text-[#565e67] text-sm">Budget</span> {rupiah(projectData?.project_budget)}
-							</p>
-							<span className="text-[#565e67]">•</span>
-							<p className="font-mono text-[#565e67] text-sm">{projectData?.expected_finish_date}</p>
-							<span className="text-[#565e67]">•</span>
-							<p className="text-base font-mono">{projectData?.tags}</p>
+			<section className="overflow-hidden bg-white py-11 font-poppins dark:bg-gray-800">
+				<div className="max-w-6xl px-4 py-4 mx-auto lg:py-8 md:px-6">
+					<div className="flex flex-wrap -mx-4">
+						<div className="w-full px-4 md:w-1/2 ">
+							<div className="relative mb-6 lg:mb-10 lg:h-2/4">
+								<img src={projectData?.project_image} alt="" className="object-cover w-full lg:h-full" />
+							</div>
 						</div>
+						<div className="w-[60%] mx-auto flex flex-col gap-4">
+							<p className="font-mono text-[#565e67]">{projectData?.starting_date}</p>
+							<h1 className="text-5xl">{projectData?.project_name}</h1>
+							<div className="flex justify-start items-center gap-4">
+								{/* <img src={projectData?.project_image} className="h-14 w-14 rounded-full object-cover" /> */}
+								<p className="text-base font-mono">
+									<span className="font-mono text-[#565e67] text-sm">Budget</span> {rupiah(projectData?.project_budget)}
+								</p>
+								<span className="text-[#565e67]">•</span>
+								<p className="font-mono text-[#565e67] text-sm">{projectData?.expected_finish_date}</p>
+								<span className="text-[#565e67]">•</span>
+								<p className="text-base font-mono">{projectData?.tags}</p>
+							</div>
+						</div>
+						<div className="border-b my-8 w-[70%] mx-auto"></div>
+						<img src={projectData?.project_image} className="rounded-3xl" />
+						<p className="text-base font-mono">{projectData?.description_project}</p>
 					</div>
-					<div className="border-b my-8 w-[70%] mx-auto"></div>
-					<img src={projectData?.project_image} className="rounded-3xl" />
-					<p className="text-base font-mono">{projectData?.description_project}</p>
 				</div>
 			</section>
 		</>
